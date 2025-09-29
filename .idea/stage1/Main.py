66feab024fbc17ccbbc1a2b60cf00c0b7e37d8ef
Milosch Fuerstenberg <20007@nordakademie.de>
2025-09-29 22:40:39 +0100

    -(Crawler) Convert txt files to JSON format upon download -(Indexer) Added inverted indexer that tokenizes, deletes stopwords and saves index structure as JSON

--- .idea/stage1/Main.py
+++ .idea/stage1/Main.py
@@ -1,1 +1,12 @@
-print("Hello World")
+from crawler import download_book
+from indexer import build_inverted_index, save_index
+
+if __name__ == "__main__":
+    # example list
+    book_ids = [76921, 76922, 76947]
+
+    for book_id in book_ids:
+        download_book(book_id)
+
+    index = build_inverted_index()
+    save_index(index)
